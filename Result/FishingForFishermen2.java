b5c00c4f1a97abde177db452c2f384fb5492e882
Vladimir Sorokin <sorokinv@hotmail.com>
2017-09-01 17:02:39 +1000

    XGB LB=888703.52

---
+++ Result/FishingForFishermen2.java
@@ -0,0 +1,9 @@
+public class FishingForFishermen2{
+    public String getAnswerURL(){
+        // return "https://drive.google.com/uc?export=download&id=XXXXXXXX";
+        // return "https://drive.google.com/open?id=0B0Xy8wvkJVSNUGJzbUs3WS1CSE0"
+        //String id = "0B0Xy8wvkJVSNUGJzbUs3WS1CSE0";
+        String id = "0B0Xy8wvkJVSNMEJSWnFCTEw1Vms";
+        return "https://drive.google.com/uc?export=download&id=" + id;
+    }
+}
